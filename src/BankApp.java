430621bdb673466f6bc778a8fa94989360cc06e0
Zoe Woods <me@etwo.dev>
2025-07-25 10:00:39 +0100

    chore: fix

--- src/BankApp.java
+++ src/BankApp.java
@@ -51,7 +51,8 @@ public class BankApp {
       System.out.println("4. Send Money");
       System.out.println("5. List Accounts");
       System.out.println("6. Close Account");
-      System.out.println("7. Back to Main Menu");
+      System.out.println("7. List account details");
+      System.out.println("8. Back to Main Menu");
       System.out.print("Choose an option: ");
 
       int choice;
